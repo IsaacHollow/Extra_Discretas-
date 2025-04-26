42441a52c4a3730f49ea47fd23a76add9ae2aa67
IsaacHollow <isaac.sibaja.cortes@est.una.ac.cr>
2025-04-25 21:37:26 -0600

    Hice una clase de prueba

--- BuscaMinas_Discretas/BuscaMinas_Discretas/BuscaMinas_Discretas.py
+++ BuscaMinas_Discretas/BuscaMinas_Discretas/BuscaMinas_Discretas.py
@@ -1,3 +1,18 @@
 
-#Hola Mundo JAJAJ
+class MiClase:
+  
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
 
+
+    def saludar(self):
+        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} anos.")
+
+
+if __name__ == "__main__":
+    
+    persona = MiClase("Isaac", 21)
+    
+   
+    persona.saludar()
